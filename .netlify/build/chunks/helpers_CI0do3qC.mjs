593fad85579f65bcd55972ea4d2356a91a8e19ac
RamonZhou <deluxurous@gmail.com>
2024-11-07 22:59:18 -0600

    remove .nettify

--- .netlify/build/chunks/helpers_CI0do3qC.mjs
+++
@@ -1,9 +0,0 @@
-function formatDate(date) {
-  return date.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-  });
-}
-
-export { formatDate as f };
--- .netlify/v1/functions/ssr/.netlify/build/chunks/post2_CddsuZAh.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./post2_D5NWd1Jd.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = ["@font-face{font-family:CabinetGrotesk;src:url(/fonts/CabinetGrotesk-Variable.ttf) format(\"truetype-variations\");font-weight:400;font-style:normal;font-display:swap;font-weight:100 1000}@font-face{font-family:Satoshi;src:url(/fonts/Satoshi-Variable.ttf) format(\"truetype-variations\");font-weight:400;font-style:normal;font-display:swap;font-weight:100 1000}body{margin:0;font-family:Satoshi,sans-serif;-webkit-font-smoothing:antialiased}b{font-weight:900!important}\n"];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
