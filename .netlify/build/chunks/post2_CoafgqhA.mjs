88d66bff1cbab1671b054e7acaf6ae552ac53ec7
RamonZhou <deluxurous@gmail.com>
2024-11-08 00:05:55 -0600

    Add meta data for QQ & Wechat

--- .netlify/build/chunks/post2_CoafgqhA.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./post2_DrNXls4N.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = ["@font-face{font-family:CabinetGrotesk;src:url(/fonts/CabinetGrotesk-Variable.ttf) format(\"truetype-variations\");font-weight:400;font-style:normal;font-display:swap;font-weight:100 1000}@font-face{font-family:Satoshi;src:url(/fonts/Satoshi-Variable.ttf) format(\"truetype-variations\");font-weight:400;font-style:normal;font-display:swap;font-weight:100 1000}body{margin:0;font-family:Satoshi,sans-serif;-webkit-font-smoothing:antialiased}b{font-weight:900!important}\n"];
-					const collectedScripts = [{"props":{"src":"/_astro/BasicLayout.astro_astro_type_script_index_0_lang.l0sNRNKZ.js","type":"module"},"children":""}];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
